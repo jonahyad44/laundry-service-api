b7ffefea0ac5ab06e0855a697368eacb1cd14524
jonahyad44 <jonahya.daniel@gmail.com>
2023-12-03 18:49:51 -0500

    changed port

--- index.ts
+++ index.ts
@@ -9,7 +9,7 @@ const jwt = require("jsonwebtoken");
 require("dotenv").config();
 const cookieParser = require("cookie-parser");
 const bodyParser = require('body-parser');
-const port = 3000
+const port = 5173
 
 
 
